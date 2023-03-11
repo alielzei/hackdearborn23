7fe3d933cc25654310b85462fba10bb82482ec94
Ali Elzein <alielzein753@gmail.com>
2023-03-10 19:16:17 -0500

    first way

--- src/index.jsx
+++ src/index.jsx
@@ -2,21 +2,31 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './global.css';
 
+function firstWay(word, key) {
+  const middle = Math.ceil(word.length / 2)
+  return <React.Fragment key={key}>
+    <b>{word.slice(undefined, middle)}</b>
+
+    {word.slice(middle, undefined)}
+
+    {" "}
+  </React.Fragment>
+}
+
 function App() {
   const [text, setText] = useState("hello this is the text that i want to input into my app")
 
   return <div>
-    <textarea 
+    <textarea
       value={text}
       onChange={(e) => setText(e.target.value)}
     />
     <div>
       <button>Transform</button>
     </div>
-    <div>{(() => {
-      console.log(text.split(' '))
-      return text
-    })()}</div>
+    <div>{
+      text.split(' ').map(firstWay)
+    }</div>
   </div>
 }
 
